ff232c4ae74cfcfcc5c33ba2007b329957e335c0
Sam Bowden <sam.bowden@techex.co.uk>
2025-02-24 15:00:12 +0000

    fix: clone media-kit and checkout v1.1.10

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -59,6 +59,6 @@ HEREDOC
 [ ! -d media-kit-android-helper ] && git clone --depth 1 --branch fix/no-dup-fd https://github.com/media-kit/media-kit-android-helper.git
 
 # media_kit
-[ ! -d media_kit ] && git clone --depth 1 --single-branch --branch main https://github.com/alexmercerind/media_kit.git
+[ ! -d media_kit ] && git clone --depth 1 --single-branch --branch media_kit-v1.1.10 https://github.com/alexmercerind/media_kit.git
 
 cd ..
