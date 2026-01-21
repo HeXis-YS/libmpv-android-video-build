ae9df5372c190c60f04155446685669cec8e7177
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-01-21 22:19:43 +0800

    Remove unused scripts

--- buildscripts/scripts/fftools_ffi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-unset CC CXX # meson wants these unset
-
-$_MESON
-
-$_NINJA
-DESTDIR="$prefix_dir" $_NINJA install
--- buildscripts/scripts/libvpx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-unset CC CXX # meson wants these unset
-
-$_MESON \
-	-Ddefault_library=static \
-	-Dcpu_features_path="$ANDROID_NDK_LATEST_HOME/sources/android/cpufeatures"
-
-$_NINJA
-DESTDIR="$prefix_dir" $_NINJA install
